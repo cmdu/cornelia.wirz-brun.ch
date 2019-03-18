685a1a62902c1bfa71ac3c671890d6b078fe672a
cmdu <tobias@2551.cc>
2019-03-18 11:02:02 +0100

    Initial commit

---
+++ js/show-hide.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	$("li a").on('click', function(e) {
+        //- prevent default behaviour of the anchor tag
+        e.preventDefault();
+        //- save the data attribute for the anchor tag that was clicked
+        var page = $(this).data('page');
+        $("header, main, footer").fadeOut('fast', function() {
+        	$(this).addClass('rockbottom');
+        	$('#pages .page[data-page="'+page+'"]').fadeIn('fast').removeClass('hide');
+				$('#pages .page[data-page="'+page+'"]').on('click', function() {
+	    			$(this).fadeOut('fast', function() {
+	    				$('header, main, footer').fadeIn('fast').removeClass('rockbottom');
+    			});
+			});
+        });
+    });
+});
---
+++ js/underline-randomizer.js
@@ -0,0 +1,7 @@
+$( "a" ).each(function( index ) {
+$( this ).css("border-bottom", "2px dashed " + randomColor());
+});
+
+$( "#spezi" ).each(function( index ) {
+$( this ).css("border-bottom", "2px solid " + randomColor());
+});
